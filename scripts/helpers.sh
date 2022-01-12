d5e20bf97c26275e455938aa4ef741c34bd628c4
MaxTorbeev <me@maxtor.name>
2022-01-12 12:56:58 +0300

    clean code

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# path to root repository folder
+rootpath="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; cd "../" >/dev/null 2>&1 ; pwd -P )"
+
 # set default branch
 branch="release"
 
@@ -40,30 +43,12 @@ function ignoreErrors()
     set +e
 }
 
-# path to root repository folder
-rootpath="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; cd "../" >/dev/null 2>&1 ; pwd -P )"
-
-#while getopts ":b:r:" opt; do
-#    case $opt in
-#      b) branch="$OPTARG" ;;
-#      r) repos="$OPTARG" ;;
-#      \?) echo "Invalid option -$OPTARG" >&2
-#      exit 1 ;;
-#    esac
-#
-#    case $OPTARG in
-#      -*) echo "Option $opt needs a valid argument"
-#      exit 1 ;;
-#    esac
-#  done
-
 # Set git-config values known to fix git errors
 git config core.eol lf
 git config core.autocrlf false
 git config fsck.zeroPaddedFilemode ignore
 git config fetch.fsck.zeroPaddedFilemode ignore
 git config receive.fsck.zeroPaddedFilemode ignore
-
 
 if [ "${1}" != "--source-only" ]; then
     main "${@}"
