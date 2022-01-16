120382cd076c247b2961b203e5acf746be6ae8a5
MaxTorbeev <me@maxtor.name>
2022-01-16 09:37:37 +0300

    fix

--- scripts/git-merge.sh
+++ scripts/git-merge.sh
@@ -61,7 +61,7 @@ try
   fi
 
   if [ -n "$( git -C "${rootpath}" diff -b -w --name-only "${current}" "${current_remote}" | cat )" ]; then
-    echo "Ошибка. Ветки ${current} не совпадает с удаленным репозиторием";
+    echo "Ошибка. Ветки ${current} не совпадает с удаленным репозиторием ${current_remote}";
     exit 0;
   fi
 
